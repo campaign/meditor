138004cd536ea481503978eb4af6239a68688c75
campaign <zhanyi1022@gmail.com>
2013-02-21 19:17:14 +0800

    by zhanyi

---
+++ _src/zepto.hammer.js
@@ -0,0 +1,21 @@
+(function($){
+    var events =  ['hold','tap','doubletap','transformstart','transform','transformend','dragstart','drag','dragend','swipe','release'];
+    events.forEach(function(m){
+        $.fn[m] = function(callback){
+            return this.each(function(){
+                var hammer = new Hammer(this), el = this;
+                hammer['on' + m] = function(){
+                    $(el).bind(m, callback)
+                }
+            })
+        }
+    });
+    var orgOn = $.fn.on;
+    $.fn.on = function(event, selector, callback){
+        if(events.indexOf(event)!=-1){
+            $.fn[event].call(this,selector);
+        }else{
+            orgOn.call(this,event, selector, callback)
+        }
+    }
+})(Zepto);
