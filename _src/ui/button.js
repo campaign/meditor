ead7a7bf7591b7aa3e5ad95a836e48ed5ea2f6b8
liaoxuezhi <liaoxuezhi@baidu.com>
2013-02-25 16:26:14 +0800

    Button

--- _src/ui/button.js
+++ _src/ui/button.js
@@ -1,46 +1,51 @@
-///import core
-///import uicore
-///import ui/stateful.js
-(function (){
-    var utils = baidu.editor.utils,
-        UIBase = baidu.editor.ui.UIBase,
-        Stateful = baidu.editor.ui.Stateful,
-        Button = baidu.editor.ui.Button = function (options){
-            this.initOptions(options);
-            this.initButton();
-        };
-    Button.prototype = {
-        uiName: 'button',
-        label: '',
-        title: '',
-        showIcon: true,
-        showText: true,
-        initButton: function (){
-            this.initUIBase();
-            this.Stateful_init();
+(function($, ui){
+    ui.define('button', {
+        _options: {
+            key: '',
+            title: '按钮',
+            diabled: false,
+            highlight: false
         },
-        getHtmlTpl: function (){
-            return '<div id="##" class="edui-box %%">' +
-                '<div id="##_state" stateful>' +
-                 '<div class="%%-wrap"><div id="##_body" unselectable="on" ' + (this.title ? 'title="' + this.title + '"' : '') +
-                 ' class="%%-body" onmousedown="return false;" onclick="return $$._onClick();">' +
-                  (this.showIcon ? '<div class="edui-box edui-icon"></div>' : '') +
-                  (this.showText ? '<div class="edui-box edui-label">' + this.label + '</div>' : '') +
-                 '</div>' +
-                '</div>' +
-                '</div></div>';
-        },
-        postRender: function (){
-            this.Stateful_postRender();
-            this.setDisabled(this.disabled)
-        },
-        _onClick: function (){
-            if (!this.isDisabled()) {
-                this.fireEvent('click');
-            }
-        }
-    };
-    utils.inherits(Button, UIBase);
-    utils.extend(Button.prototype, Stateful);
 
-})();
+        _create: function(){
+            var opt = this._options, key = opt.key;
+            this.root($('<div class="mui-button'+(key?' mui-button-'+key:'')+'" title="'+opt.title+'">' +
+                '<span class="icon"></span>'+
+                '</div>'));
+        },
+
+        _init: function(){
+            var el = this.root(), opt = this._options, _eH = $.proxy(this._eventHandler, this);
+            el.on('click', _eH);
+            this.enable(!opt.disabled).highlight(opt.highlight);
+        },
+
+        _eventHandler: function(e){
+            if(this.isEnable()){
+                this.trigger('buttonclick', $.extend({}, this.option()));
+            }
+        },
+
+        enable: function(flag){
+            var el = this.root();
+            el[flag?'removeClass':'addClass']('mui-state-disable').highlight(flag?'mui-state-hover':null);
+            return this;
+        },
+
+        isEnable: function(){
+            var el = this.root();
+            return !el.hasClass('mui-state-disable');
+        },
+
+        highlight: function(flag){
+            var el = this.root();
+            el[flag?'addClass':'removeClass']('mui-state-highlight');
+            return this;
+        },
+
+        isHighlight: function(){
+            var el = this.root();
+            return !el.hasClass('mui-state-highlight');
+        }
+    });
+})(Zepto, baidu.meditor.ui);
