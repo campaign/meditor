9f63b891425a3ac0e7f046810f333be1222a1c90
zhoumm1008 <zhoumeimei@baidu.com>
2013-02-26 17:57:46 +0800

    [newFeature] toolbar.js toolbar.css

--- _src/ui/toolbar.js
+++ _src/ui/toolbar.js
@@ -39,13 +39,155 @@
 
 })();*/
 
-(function ($) {
-    baidu.meditor.ui.define('Toolbar', {
-        fn: function () {},
-        attr:1,
+
+(function ($, ui) {
+    ui.define('ScrollBox', {
+        _options: {
+            showCount: 4,
+            items: []
+        },
         _create: function () {
-            this._el = $('<div></div>').appendTo('body');
+            var me = this,
+                opts = me._options, $scroller;
+
+            me.root($('<div class="mui-scrollbox"></div>').append($scroller = $('<div class="mui-scrollBox-scroller"></div>'))),
+            $.each(opts.items, function (key, item) {
+                me._isInstance(item) && item.render($scroller);
+            });
+            me._$scroller = $scroller;
+
+            return me;
+        },
+        _getSize: function () {
+            var me = this,
+                widths = [],
+                opts = me._options,
+                height = 0, width;
+
+            $.each(opts.items, function (i, item) {
+                var $item = item.root();
+                widths.push($item.width());
+                i < opts.showCount && (height += $item.height() + parseInt($item.css('margin-top')) + parseInt($item.css('margin-bottom')));
+            });
+            return [Math.max.apply(Math, widths), height];
+        },
+        _enableScroll: function () {
+            var me = this,
+                size = me._getSize();
+
+            me.iscroll = me.root().css({
+                width: size[0],
+                height: size[1]
+            }).iscroll().iscroll('_refresh');
+            return me;
+        },
+        addItem: function (item) {
+            var me = this;
+            me._isInstance(item) && item.render(me.root()).iscroll._refresh();
+            return me;
+        },
+        _isInstance: function (instance) {
+            return instance instanceof ui.Button || instance instanceof ui.Group;
+        },
+        render: function (container) {
+            var me = this;
+            me.root().appendTo(container || document.body);
+            return me._enableScroll();
         }
     });
 
-})(Zepto);
+    ui.define('Group', {
+        _options: {
+            items: []
+        },
+        _init: function () {
+            var me = this;
+
+            me.$super('_init');
+            me.root().on('buttonClick', $.proxy(me._showPopup, me));
+        },
+
+        _showPopup: function () {
+            var me = this,
+                items = this._options.items,
+                $subBarWrap = $('<div class="mui-group-wrap"></div> ');
+
+            me._$subBarWrap = $subBarWrap;
+            $.each(items, function (i, item) {
+                me.addItem(item);
+            });
+            me._subBar = ui.Popup({
+                content: $subBarWrap
+            });
+        },
+        _isInstance: function (instance) {
+            return instance instanceof ui.Button || instance instanceof ui.Group || instance instanceof ui.ScrollBox;
+        },
+        addItem: function (item) {
+            this._isInstance(item) && item.render(this._$subBarWrap);
+            return this;
+        }
+    }, ui.Button);
+
+    ui.define('Toolbar', {
+        _options: {
+            mode: 'swipe',    //'swipe' || 'static',
+            items: [],
+            useFix: false
+        },
+        _create: function () {
+            var me = this,
+                opts = me._options;
+
+            me.root($('<div class="mui-toolbar"></div>').append(me._$boxWrap = $('<div class="mui-toolbar-wrap"></div>').append(me._$toolBox = $('<div class="mui-toolbar-toolBox"></div>'))));
+            opts.mode == 'swipe' && me._$boxWrap.prepend(me._$mask = $('<div class="mui-toolbar-mask"><span class="mui-toolbar-showArrow"></span></div>'));
+            $.each(opts.items, function (key, item) {
+                me.addItem(item);
+            });
+        },
+        _init: function () {
+            var me = this;
+            me._options.mode == 'swipe' ? me.root().on('swipeLeft swipeRight', $.proxy(me._eventHandler, me)) : me.show();
+            return this;
+        },
+        _eventHandler: function (e) {
+            var me = this;
+            alert(e.type);
+            switch (e.type) {
+                case 'swipeRight':
+                    me.hide(function () {
+                        me._$boxWrap.css('-webkit-transform', 'translateX(0px)');
+                    });
+                    break;
+                case 'swipeLeft':
+                    me.show(function () {
+                        me._$boxWrap.css('-webkit-transform', 'translateX(-' + me._$toolBox.width() + 'px)');
+                    });
+                    break;
+            }
+        },
+        _isInstance: function (instance) {
+            return instance instanceof ui.Button || instance instanceof ui.Group || instance instanceof ui.ScrollBox;
+        },
+        _setVisible: function (fn, isShow) {
+            $.isFunction(fn) ? fn.call(this) : this.root()[isShow ? 'show' : 'hide']();
+            if ($.isFunction(fn)) {
+                fn.call(this);
+            } else {
+                this._$mask[isShow ? 'hide' : 'show']();
+                this._$toolBox[isShow ? 'hide' : 'show']();
+            }
+            return this;
+        },
+        show: function (fn) {
+            return this._setVisible(fn, true);
+        },
+        hide: function (fn) {
+            return this._setVisible(fn, false);
+        },
+        addItem: function (item) {
+            this._isInstance(item) && item.render(this._$toolBox);
+            return this;
+        }
+    });
+})(Zepto, baidu.meditor.ui);
