9c06fd0ca1df9bf7f393cc3d20049eaf475e8856
liaoxuezhi <liaoxuezhi@baidu.com>
2013-03-05 14:01:34 +0800

    添加功能

--- _src/adapter/buttons.js
+++ _src/adapter/buttons.js
@@ -1,21 +1,48 @@
-;(function($, ns){
+;(function($, ns, undefined){
     var ui = ns.ui;
 
+    //translate
+    function _t(cmd){
+        return {
+            align: 'justify',
+            orderedlist: 'insertorderedlist',
+            unorderedlist: 'insertunorderedlist'
+        }[cmd] || cmd;
+    }
+
     ns.registerUI(
-        'undo redo bold italic underline strikethrough '+
+        'undo redo bold italic underline strikethrough ' +
+        'alignleft aligncenter alignright alignjustify ' +
+        'orderedlist unorderedlist indent ' +
         'removeformat',
 
         function(editor,cmdName){
-            var btn = ui.button({
+            var btn,
+                cmd = cmdName,
+                flag = undefined;
+
+            if(/^(align)(.*)$/.test(cmdName)){
+                cmd = RegExp.$1;
+                flag = RegExp.$2;
+            }
+
+            cmd = _t(cmd);
+
+            btn = ui.button({
                 name : cmdName,
                 click:function(){
-                    editor.execCommand(cmdName)
+                    editor.execCommand(cmd, flag);
                 },
                 title:cmdName
             });
 
             editor.on('selectionchange',function(){
-                var state = editor.queryCommandState(cmdName);
+                var state = editor.queryCommandState(cmd);
+
+                if(flag && -1 !== state) {
+                    state = editor.queryCommandValue(cmd) === flag ? 1 : 0;
+                }
+
                 btn.highlight(state == 1).enable(state != -1);
             });
             return btn;
@@ -25,12 +52,10 @@
     //临时, 为了让按钮能正常显示.
     ns.registerUI(
         [ 'formatmatch',
-            'orderedlist', 'unorderedlist', 'indent',
-            'alignleft', 'aligncenter', 'alignright', 'alignjustify',
             'fontfamily', 'fontsize', 'fontcolor', 'bgcolor',
             'image', 'attach',
             'touppercase', 'tolowercase',
-            'subscript', 'superscript', 'source', 'indent', 'outdent',
+            'subscript', 'superscript', 'source', 'outdent',
             'blockquote', 'pasteplain', 'pagebreak',
             'selectall', 'print', 'preview', 'horizontal', 'time', 'date', 'unlink',
             'insertparagraphbeforetable', 'insertrow', 'insertcol', 'mergeright', 'mergedown', 'deleterow',
@@ -45,4 +70,4 @@
             return btn;
         }
     );
-})(Zepto,ME)
+})(Zepto, ME)
