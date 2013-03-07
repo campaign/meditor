66cdba85674ae17b69860f22770622b87697c0cb
liaoxuezhi <liaoxuezhi@baidu.com>
2013-03-07 13:28:47 +0800

    修复ui bug

--- _src/ui/ui.js
+++ _src/ui/ui.js
@@ -71,7 +71,7 @@
             opt = me._options = $.extend({
                 theme: 'default',
                 watchRender: false
-            }, me._options, opts);
+            }, me._options, opts || {});
             me._create();
             if(!(el = me.root())){
                 throw new Error('组件的_create方法里面必须创建并保存根元素');
