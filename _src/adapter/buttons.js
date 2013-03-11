f02ebe31594e0e10fe69868869b50045fd0c44d5
liaoxuezhi <liaoxuezhi@baidu.com>
2013-03-11 12:03:26 +0800

    moverange

--- _src/adapter/buttons.js
+++ _src/adapter/buttons.js
@@ -12,7 +12,6 @@
 
     ns.registerUI(
         'undo redo bold italic underline strikethrough ' +
-        'lcursorbackward lcursorforward mcursor rcursorbackward rcursorforward ' +
         'alignleft aligncenter alignright alignjustify ' +
         'orderedlist unorderedlist indent ' +
         'removeformat',
@@ -71,4 +70,35 @@
             return btn;
         }
     );
-})(Zepto, ME)
+})(Zepto, ME);
+
+(function($, ns){
+    var ui = ns.ui;
+
+    ns.registerUI('mcursor', function(editor, cmdName){
+        return ui.button({
+            name : cmdName,
+            title:cmdName
+        });
+    });
+
+    ns.registerUI('lcursorbackward lcursorforward rcursorbackward rcursorforward',
+    function(editor, cmdName){
+        var dir = /backward$/.test(cmdName)?'left':'right',
+            cmd = 'moverange',
+            end = /^rcursor/.test(cmdName),
+            btn = ui.button({
+                name : cmdName,
+                click:function(){
+                    editor.execCommand(cmd, dir, end);
+                },
+                title:cmdName
+            });
+
+        editor.on('selectionchange',function(){
+            var state = editor.queryCommandState(cmd, dir, end);
+            btn.highlight(state == 1).enable(state != -1);
+        });
+        return btn;
+    });
+})(Zepto, ME);
