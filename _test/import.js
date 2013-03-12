3a5bc5e494b5a547500b5c150c7b2fe03ea5ff07
yancen <dongyancen@baidu.com>
2013-03-12 18:45:53 +0800

    [case]移植用例

--- _test/import.js
+++ _test/import.js
@@ -3,4 +3,15 @@
 ///import hammer;
 ///import zepto.hammer;
 ///import meditor;
+
+///import core/utils;
+///import core/EventBase;
+///import core/domUtils;
+///import core/Range;
+///import core/Selection;
+///import core/Editor;
+///import core/filternode;
+///import core/node;
+///import core/htmlparser;
 ///import core/extend;
+
