09bd5461d3977612153eee6ae93bd738b3224ae3
campaign <zhanyi1022@gmail.com>
2013-03-13 11:37:19 +0800

    by zhanyi

--- _src/plugins/moverange.js
+++ _src/plugins/moverange.js
@@ -1,31 +1,32 @@
-ME.plugins['moverange'] = function(){
-    var me = this;
-    me.commands['moverange'] = {
-        execCommand : function( cmd,dir ) {
-            var me = this,
-                rng = me.selection.getRange();
-            rng.shrinkBoundary();
-            var address = rng.createAddress();
-            try{
-                if(rng.collapsed){
-                    rng.shrinkBoundary();
-                    rng.startOffset = rng.startOffset + (dir == 'left' ? -1 : 1);
-                    rng.collapse(true);
-                    rng.select();
-                }else{
-                    rng.shrinkBoundary();
-                    if(dir == 'left'){
-                        rng.startOffset = rng.startOffset -1;
+;
+(function ($) {
+    ME.plugins['moverange'] = function () {
+        var me = this;
+        me.commands['moverange'] = {
+            execCommand:function (cmd, dir,isStart) {
+                var me = this,
+                    rng = me.selection.getRange();
+                var address = rng.createAddress();
+                try{
+                    if(rng.collapsed){
+                        rng.shrinkBoundary();
+                        rng.startOffset = rng.startOffset + (dir == 'left' ? -1 : 1);
+                        rng.setCursor(true,true)
                     }else{
-                        rng.endOffset = rng.endOffset +1;
-                    }
-                    rng.select();
-                }
-            }catch(e){
-                rng.moveToAddress(address).select()
-            }
-        },
-        notNeedUndo:1
-    };
-};
+                        rng.shrinkBoundary();
+                        if(isStart){
+                            rng.startOffset = rng.startOffset + (dir = 'left' ? 1 : -1);
 
+                        }else{
+                            rng.endOffset = rng.endOffset + (dir = 'left' ? -1 : 1);
+                        }
+                        rng.select();
+                    }
+                } catch (e) {
+                    rng.moveToAddress(address).select()
+                }
+            },
+            notNeedUndo:1
+        };
+    };
+})(Zepto)
